466a23ba4dd64d16c74874d018452c91b185d85b
yorunoken <me@yorunoken.com>
2026-01-16 21:32:59 +0300

    skip serialization on nullable values

--- src/models/profile.rs
+++ src/models/profile.rs
@@ -40,8 +40,10 @@ pub struct ProfileSet {
     pub store: HashMap<String, ProfileConfig>,
 
     #[serde(rename = "mills")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub mills: Option<i64>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub units: Option<String>,
 
     #[napi(js_name = "createdAt")]
@@ -52,7 +54,9 @@ pub struct ProfileSet {
 #[napi(object)]
 pub struct ProfileConfig {
     pub dia: f64,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub carbs_hr: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub delay: Option<f64>,
     pub timezone: String,
     pub units: String,
